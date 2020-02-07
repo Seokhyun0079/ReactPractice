6683c1964fb3628c3e3c6b908cf58015dc4cbde9
seokhyun0079 <seokhyun0324@gamil.com>
2020-02-07 13:51:48 +0900

    first commit

---
+++ 20200217react/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const name = 'react';
+  return (
+    <div>
+      <div className="react">
+        {name}
+      </div>
+      <h1>delyosugi</h1>
+      <h1>it is</h1>
+      <h2>strangecode</h2>
+    </div>
+  );
+}
+
+export default App;
