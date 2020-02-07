40955efdbfe5e98509182057b275db0dd0b3f3f8
seokhyun0079 <seokhyun0324@gamil.com>
2020-02-07 14:12:43 +0900

    first MyComponent

--- 20200217react/src/App.js
+++ 20200217react/src/App.js
@@ -4,13 +4,8 @@ import './App.css';
 function App() {
   const name = 'react';
   return (
-    <div>
-      <div className="react">
-        {name}
-      </div>
-      <h1>delyosugi</h1>
-      <h1>it is</h1>
-      <h2>strangecode</h2>
+    <div className="react">
+      {name}
     </div>
   );
 }
---
+++ 20200217react/src/MyComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyComponent = ()=> {
+    return <div>my first Component</div>;
+}
+
+export default MyComponent;
