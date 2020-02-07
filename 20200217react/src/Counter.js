ab99d299edc980f8784524635277f26b82c9f9fe
seokhyun0079 <seokhyun0324@gamil.com>
2020-02-07 16:27:05 +0900

    SetState

--- 20200217react/src/Counter.js
+++ 20200217react/src/Counter.js
@@ -1,22 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Counter extends Component{
-    constructor(props){
+class Counter extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-            number : 0
+            number: 0,
+            fixedNumber: 0
         };
     }
-    render(){
-        const { number } = this.state;
-        return(
+    render() {
+        const { number, fixedNumber } = this.state;
+        return (
             <div>
                 <h1>{number}</h1>
-                <button onClick = {
-                    ()=>{
+                <h2>never changed value : {fixedNumber}</h2>
+                <button onClick={
+                    () => {
                         this.setState({
-                            number : number + 1
-                        });
+                            number: number + 1
+                        },
+                            () => {
+                                console.log('さっきsetStateが呼び出された。');
+                                console.log(this.state);
+                            }
+                        );
                     }
                 }>
                     +1
