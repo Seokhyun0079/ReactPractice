f086e27fb41b6ea6388a66eb784a7efbc268e755
seokhyun0079 <seokhyun0324@gamil.com>
2020-02-10 16:37:01 +0900

    ValidationSample

--- 20200217react/src/App.js
+++ 20200217react/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import EvnetPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
 
 const App = () => {
-  return <EvnetPractice/>;
+  return <ValidationSample/>;
 }
 
 export default App;
