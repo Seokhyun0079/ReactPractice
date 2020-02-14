062e4a58dbee4fcff39126a718724a0504e3f328
seokhyun0079 <seokhyun0324@gamil.com>
2020-02-14 17:35:12 +0900

    today's last commit

---
+++ router-tutorial/src/Home.js
@@ -0,0 +1,13 @@
+import Reacct from 'react';
+
+
+const Home = () =>{
+    return (
+        <div>
+            <h1>ホーム</h1>
+            <p>ホーム、そのページは最初に見せられるページ</p>
+        </div>
+    )
+}
+
+export default Home;
