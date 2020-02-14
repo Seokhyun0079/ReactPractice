4d4c186b748294296710e74b3fbf23d71f39dafd
seokhyun0079 <seokhyun0324@gamil.com>
2020-02-14 17:19:56 +0900

    router tutorial project setting

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,24 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useCallback } from 'react';
+import produce from 'immer';
 
-function App() {
+const App = () => {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: '', usename: '' });
+  const [data, setData] = useState({ array: [], uselessValue: null });
+
+  // const onChange = useCallback(
+  //   e => {
+  //     const { name, value } = e.target;
+  //     setForm({
+  //       ...form,
+  //       [name]: [value]
+  //     });
+  //   },
+  //   [form]
+  // );
+  // change like this
+
+  const onChange = useCallback(
+    e => {
+      const { name, value} = e.target;
+      setForm(
+        produce(form, draft => {
+          draft[name] = value;
+        })
+      );
+    },
+    [form]
+  )
+
+  // const onSubmit = useCallback(
+  //   e => {
+  //     e.preventDefault();
+  //     const info = {
+  //       id: nextId.current,
+  //       name: form.name,
+  //       username: form.username
+  //     };
+  //     setData({
+  //       ...data,
+  //       array: data.array.concat(info)
+  //     });
+
+  //     setForm({
+  //       name: '',
+  //       username: ''
+  //     });
+
+  //     nextId.current += 1;
+  //   },
+  //   [data, form.name, form.username]
+  // );
+
+  // change like this
+
+
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      const info = {
+        id : nextId.current,
+        name : form.name,
+        username : form.username
+      };
+
+      setData(
+        produce(data, draft =>{
+          draft.array.push(info);
+        })
+      );
+
+      setForm({
+        name : '',
+        username : ''
+      });
+      nextId.current += 1;
+    },
+    [data, form.name, form.username]
+  );
+
+  // const onRemove = useCallback(
+  //   id => {
+  //     setData({
+  //       ...data,
+  //       array: data.array.filter(info => info.id !== id)
+  //     });
+  //   },
+  //   [data]
+  // );
+
+  // change like this
+
+  const onRemove = useCallback(
+    id => {
+      setData(
+        produce(data, draft=>{
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+        })
+      );
+    },
+    [data]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input name="username"
+          placeholder="id"
+          value={form.username}
+          onChange = {onChange}
+        />
+        <input name ="name"
+        placeholder="name"
+        value={form.name}
+        onChange={onChange}/>
+        <button type = "submit">登録</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map(info =>(
+            <li key={info.id}
+            onClick={()=> onRemove(info.id)}
+            >
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
