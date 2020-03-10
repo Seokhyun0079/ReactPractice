cbaaeb759d7ab9d79513494281fcbfc79592a1ae
Seokhyun0079 <seokhyun0324@gmail.com>
2020-03-10 23:11:32 +0900

    새 프로젝트 만들고나서 일단 커밋

--- new-viewer/src/components/NewsList.js
+++ new-viewer/src/components/NewsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 import axios from "axios";
+import usePromise from "../lib/usePromise";
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -17,34 +18,28 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === "all" ? "" : `&category=${category}`;
-        console.log(query);
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=2dd37ceed1c0492ba8a4577fa9ec5661`
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === "all" ? "" : `&category=${category}`;
+    console.log(query);
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=2dd37ceed1c0492ba8a4577fa9ec5661`
+    );
   }, [category]);
 
+  console.log(response);
   if (loading) {
     return <NewsListBlock>대기중...</NewsListBlock>;
   }
 
-  if (!articles) {
+  if (!response) {
     return null;
   }
+
+  if (error) {
+    return <NewsListBlock>에러발생!</NewsListBlock>;
+  }
+
+  const { articles } = response.data;
 
   return (
     <NewsListBlock>
