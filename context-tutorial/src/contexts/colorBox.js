011218e8c2ddc7840fe41cdcb57ec829bfc0c9fa
Seokhyun0079 <seokhyun0324@gmail.com>
2020-03-15 22:49:26 +0900

    뭔가 했음

--- context-tutorial/src/contexts/colorBox.js
+++ context-tutorial/src/contexts/colorBox.js
@@ -1,19 +1,25 @@
-import react from "react";
+import React, { useContext } from "react";
 import ColorContext from "../contexts/color";
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorContext.Consumer>
-      {value => (
-        <div
-          style={{
-            width: "64px",
-            height: "64px",
-            background: value.color
-          }}
-        />
-      )}
-    </ColorContext.Consumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor
+        }}
+      />
+    </>
   );
 };
 
