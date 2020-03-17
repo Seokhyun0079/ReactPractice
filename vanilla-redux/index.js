a7d2c4a1bf972b9f9e3bc77b3a0d6f849f2cf4ca
Seokhyun0079 <seokhyun0324@gmail.com>
2020-03-17 22:55:25 +0900

    머임 이 책 도대체 언제 끝냄 ㅅㅂ

--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,1 +1,55 @@
-console.log("hello parcel");
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+import { createStore } from "redux";
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = differnce => ({ type: INCREASE, differnce });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+  toggle: false,
+  counter: 0
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.coutner + action.differnce
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const render = () => {
+  const state = store.getState();
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+  counter.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
