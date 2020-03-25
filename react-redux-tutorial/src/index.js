a09ff6a48ddc9fdfd3304df5e8120948a51f2a0c
Seokhyun0079 <seokhyun0324@gmail.com>
2020-03-25 23:45:43 +0900

    오오늘 좀 알차게한듯 벌써 11시 45분임

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import { composeWithDevTools } from "redux-devtools-extension";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import rootReducer from "./moudules";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
