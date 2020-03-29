e289a0cf9abe7c564756b15a18f4cd8dd544fa01
Seokhyun0079 <seokhyun0324@gmail.com>
2020-03-29 20:27:08 +0900

    새로운 키보드를 샀습니다 아무도 궁금하지 않겠지만요

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,37 +1,61 @@
 import React from "react";
-import { connect } from "react-redux";
-import { changeInput, insert, toggle, remove } from "../moudules/todos";
+import { connect, useSelector } from "react-redux";
+import todos, { changeInput, insert, toggle, remove } from "../moudules/todos";
 import Todos from "../components/Todos";
+import useActions from "../lib/useActions";
 
-const TodosContainer = ({
-  input,
-  todos,
-  changeInput,
-  insert,
-  toggle,
-  remove
-}) => {
+// const TodosContainer = ({
+//   input,
+//   todos,
+//   changeInput,
+//   insert,
+//   toggle,
+//   remove
+// }) => {
+//   return (
+//     <Todos
+//       input={input}
+//       todos={todos}
+//       onChangeInput={changeInput}
+//       onInsert={insert}
+//       onToggle={toggle}
+//       onRemove={remove}
+//     />
+//   );
+// };
+
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos
+  }));
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
     />
   );
 };
 
-export default connect(
-  ({ todos }) => ({
-    input: todos.input,
-    todos: todos.todos
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove
-  }
-)(TodosContainer);
+export default TodosContainer;
+
+// export default connect(
+//   ({ todos }) => ({-
+
+//     todos: todos.todos
+//   }),
+//   {
+//     changeInput,
+//     insert,
+//     toggle,
+//     remove
+//   }
+// )(TodosContainer);
