b27acf76200383d6f84914730b83a6797faa930b
Seokhyun0079 <seokhyun0324@gmail.com>
2020-04-15 21:52:06 +0900

    이 책 돈주고 산 게 아까워서라도 꾸역꾸역 공부하고 있습니다

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Component } from "react";
 import { useState } from "react";
-const SplitMe = React.lazy(() => import("./SplitMe"));
-
+import loadable from "@loadable/component";
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>,
+});
 function App() {
   const [visible, setVisivle] = useState(false);
-
+  const onClick = () => {
+    setVisivle(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={this.handleClick}>Hello React</p>
-        {SplitMe && <SplitMe />}
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
