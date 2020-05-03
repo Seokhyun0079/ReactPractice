847ec9eaeffa45bc0b268d23f9b284cbd4d0bd9b
Seokhyun0079 <seokhyun0324@gmail.com>
2020-05-03 22:43:47 +0900

    골든 위크에도 공부는 해야져;

---
+++ blog/blog-backend/src/index.js
@@ -0,0 +1,10 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(ctx => {
+    ctx.body = 'hello world';
+});
+
+app.listen(4000, ()=> {
+    console.log('Listenig to port 4000');
+});
