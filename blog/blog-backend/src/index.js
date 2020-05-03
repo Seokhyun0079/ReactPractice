2387a4290cc60f04560b43256bb4fc7034a635cd
Seokhyun0079 <seokhyun0324@gmail.com>
2020-05-04 01:04:14 +0900

    내일은 디비랑 연결할거에염 책에선 몽고지만 전 마리아로감 ㅅㄱ

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,20 +1,17 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
+
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx, next)=>{
-    console.log(ctx.url);
-    console.log(1);
-    next();
-});
+router.use('/api', api.routes());
 
-app.use((ctx, next)=> {
-    console.log(2);
-    next();
-});
+app.use(bodyParser());
 
-app.use(ctx => {
-    
-});
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, ()=> {
     console.log('Listenig to port 4000');
