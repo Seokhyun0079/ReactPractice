d68142b46846187c7641867edae902e68968c3cf
Seokhyun0079 <seokhyun0324@gmail.com>
2020-05-03 23:33:13 +0900

    파일 개마늠;

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,8 +1,19 @@
 const Koa = require('koa');
 const app = new Koa();
 
+app.use((ctx, next)=>{
+    console.log(ctx.url);
+    console.log(1);
+    next();
+});
+
+app.use((ctx, next)=> {
+    console.log(2);
+    next();
+});
+
 app.use(ctx => {
-    ctx.body = 'hello world';
+    
 });
 
 app.listen(4000, ()=> {
