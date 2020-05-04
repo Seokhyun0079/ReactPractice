1936df32a1eb82071dc3ec69fb5e4a07cce3693b
Seokhyun0079 <seokhyun0324@gmail.com>
2020-05-04 16:29:26 +0900

    몽고 디비 쓰는 것도 경험인 거 같아서 그냥 몽고디비씁니당

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const {PORT, MONGO_URI} = process.env;
+
+mongoose.connect(MONGO_URI, {useNewUrlParser : true, useFindAndModify : false})
+.then(()=>{
+    console.log('Connected to MongoDB');
+})
+.catch(e => {
+    console.error(e);
+});
 
 const api = require('./api');
 
@@ -13,6 +25,9 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, ()=> {
-    console.log('Listenig to port 4000');
+const port = PORT || 4000;
+
+app.listen(port, ()=> {
+    console.log('Listenig to port %d'
+    , port);
 });
