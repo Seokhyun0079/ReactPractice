78dca2ba041bbbda5de757fbf06fc633761849bb
Seokhyun0079 <seokhyun0324@gmail.com>
2020-05-05 00:10:49 +0900

    잠깐 휴식좀;

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -26,14 +26,45 @@ export const register = async ctx=>{
 
         const data = user.toJSON();
         delete data.hashedPassword;
-        ctx.body = data;
+        ctx.body = user.serialize();
+
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000*60*60*24*7,
+            httpOnly : true,
+        });
     }catch(e){
-        ctx.trhow(500, e);
+        ctx.throw(500, e);
     }
 };
 
 export const login = async ctx => {
-
+    const {username, password} = ctx.request.body;
+    if(!username||!password){
+        ctx.status = 401;
+        return;
+    }
+    try{
+        const user =await User.findByUsername(username);
+        if(!user){
+            ctx.status = 401;
+            return;
+        }
+        const valid = await user.checkPassword(password);
+        if(!valid){
+            ctx.status = 401;
+            return;
+        }
+        ctx.body = user.serialize();
+        
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000*60*60*24*7,
+            httpOnly : true,
+        });
+    }catch(e){
+        ctx.throw(500, e);
+    }
 };
 
 export const check = async ctx =>{
--- blog/blog-backend/src/models/user.js
+++ blog/blog-backend/src/models/user.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
     username : String,
@@ -17,6 +19,26 @@ UserSchema.methods.checkPassword = async function(password){
 
 UserSchema.statics.findByUsername = function(username){
     return this.findOne({username});
+}
+
+UserSchema.methods.serialize = function(){
+    const data = this.toJSON();
+    delete data.hashedPassword;
+    return data;
+};
+
+UserSchema.methods.generateToken = function(){
+    const token = jwt.sign(
+        {
+            _id: this.id,
+            username : this.username
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn:'7d',
+        },
+    );
+    return token;
 }
 
 const User = mongoose.model('User', UserSchema);
