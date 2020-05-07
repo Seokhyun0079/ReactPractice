12280570ee3d86c58426a3eb3b903ba87f7ff2dd
Seokhyun0079 <seokhyun0324@gmail.com>
2020-05-07 23:07:14 +0900

    오늘도 먼가 열시미햇서여

--- blog/blog-frontend/src/modules/auth.js
+++ blog/blog-frontend/src/modules/auth.js
@@ -1,41 +1,35 @@
-import { createAction, handleActions } from "redux-actions";
+import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-export const changeField = createAction(
-    CHANGE_FIELD,
-    ({form, key, value}) => ({
-        form,
-        key,
-        value
-    }),
-);
-
-export const initializeForm = createAction(INITIALIZE_FORM, form => form);
+export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
 
 const initialState = {
-    register : {
-        username : '',
-        password : '',
-        passwordConfirm : ''
-    },
-    login:{
-        username : '',
-        password : ''
-    }
+  register: {
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  },
+  login: {
+    username: '',
+    password: '',
+  },
 };
 
-const auth = handleActions({
-    [CHANGE_FIELD] : (state, {payload : {form, key, value}}) => produce(state, draft => {
+const auth = handleActions(
+  {
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, (draft) => {
         draft[form][key] = value;
+      }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
     }),
-    [INITIALIZE_FORM]: (state, {payload:form }) => ({
-        ...state,
-        [form] : initialState[form],
-    }),
-}, initialState);
-
+  },
+  initialState,
+);
 
 export default auth;
---
+++ blog/blog-frontend/src/modules/loading.js
@@ -0,0 +1,43 @@
+import loading from '../../../../learn-redux-middleware/src/modules/loading';
+import { handleActions } from 'redux-actions';
+import { initializeForm } from './auth';
+
+const START_LOADING = 'loading/START_LOADING';
+const FINISH_LOADING = 'loading/FINISH_LOADING';
+
+export const startLoading = createAction(
+  START_LOADING,
+  (requestType) => requestType,
+);
+
+export const finishLoading = createAction(
+  FINISH_LOADING,
+  (requestType) => requestType,
+);
+
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({ form, key, value }) => ({
+    form,
+    key,
+    value,
+  }),
+);
+
+const initialState = {};
+
+const loading = handleActions(
+  {
+    [START_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: true,
+    }),
+    [FINISH_LOADING]: (state, action) => ({
+      ...state,
+      [action.payload]: false,
+    }),
+  },
+  initialState,
+);
+
+export default loading;
