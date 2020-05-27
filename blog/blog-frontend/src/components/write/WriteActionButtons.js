d8752a4e8bdb585c97e3e29bb93b117172b3ce8a
Seokhyun0079 <seokhyun0324@gmail.com>
2020-05-27 23:39:11 +0900

    와 10분 따라치고 50분 어케 돌아가는지 파악에 썼는데 알고보니까 구현 안한거였음실화냐?

---
+++ blog/blog-frontend/src/components/write/WriteActionButtons.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../common/Button';
+
+const WriteActionButtonBlock = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 3rem;
+  button + button {
+    margin-left: 0.5rem;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  height: 2.125rem;
+  & + & {
+    margin-left: 0.5rem;
+  }
+`;
+
+const WriteActionButtons = ({ onCancel, onPublish }) => {
+  return (
+    <WriteActionButtonBlock>
+      <StyledButton cyan onClick={onPublish}>
+        포스트 등록
+      </StyledButton>
+      <StyledButton onClick={onCancel}>취소</StyledButton>
+    </WriteActionButtonBlock>
+  );
+};
+
+export default WriteActionButtons;
--- blog/blog-frontend/src/pages/Writepage.js
+++ blog/blog-frontend/src/pages/Writepage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Responsive from '../components/common/Responsive';
 import Editor from '../components/write/Editor';
 import TagBox from '../components/write/TagBox';
+import WriteActionsButtons from '../components/write/WriteActionButtons';
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
       <TagBox />
+      <WriteActionsButtons />
     </Responsive>
   );
 };
