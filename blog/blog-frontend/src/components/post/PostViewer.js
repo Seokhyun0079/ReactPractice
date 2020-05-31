2e8c4ce5816818c3b1e97086c871d52ce89b044e
Seokhyun0079 <seokhyun0324@gmail.com>
2020-05-31 23:00:15 +0900

    끝날 듯 끝나지 않는 나의 책

--- blog/blog-frontend/src/components/post/PostViewer.js
+++ blog/blog-frontend/src/components/post/PostViewer.js
@@ -46,28 +46,36 @@ const PostContent = styled.div`
   color: ${palette.gray[8]};
 `;
 
-const PostViewer = () => {
+const PostViewer = ({ post, error, loading }) => {
+  if (error) {
+    if (error.response && error.response.status === 404) {
+      return <PostViewerBlock>존재하지 않는 포스트입니다.</PostViewerBlock>;
+    }
+    return <PostViewerBlock>오류 발생!</PostViewerBlock>;
+  }
+  if (loading || !post) {
+    return null;
+  }
+  const { title, body, user, publishedDate, tags } = post;
   return (
     <PostViewerBlock>
       <PostHead>
-        <h1>제목</h1>
+        <h1>{title}</h1>
         <SubInfo>
           <span>
             <span>
-              <b>tester</b>
+              <b>{user.username}</b>
             </span>
-            <span>{new Date().toLocaleDateString}</span>
+            <span>{new Date(publishedDate).toLocaleDateString}</span>
           </span>
         </SubInfo>
         <Tags>
-          <div className="tag">#태그</div>
-          <div className="tag">#태그</div>
-          <div className="tag">#태그</div>
+          {tags.map((tag) => (
+            <div className="tag">#{tag}</div>
+          ))}
         </Tags>
       </PostHead>
-      <PostContent
-        dangerouslySetInnerHTML={{ __html: '<p>HTML <b>sodyd</b>dlqslek.</p>' }}
-      />
+      <PostContent dangerouslySetInnerHTML={{ __html: body }} />
     </PostViewerBlock>
   );
 };
--- blog/blog-frontend/src/pages/PostPage.js
+++ blog/blog-frontend/src/pages/PostPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import HeaderContainer from '../container/common/HeaderContainer';
-import PostViewer from '../components/post/PostViewer';
+import PostViewerContainer from '../container/post/PostViewerContainer';
 
 const PostPage = () => {
   return (
     <>
       <HeaderContainer />
-      <PostViewer />
+      <PostViewerContainer />
     </>
   );
 };
